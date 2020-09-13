cd9acee3617cff8534f9c903d9bbb45818a2db92
zzzonki <zzzonki@gmail.com>
2020-09-13 14:54:51 +0300

    h1 + fixes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import CardField from '../CardField'
+import './style.css'
 
 function App() {
   return (
-    <div>
+    <div className="wrapper">
+      <h1 className="h1">Ты сегодня покормил кота?</h1>
       <CardField />
     </div>
   );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -10,26 +10,21 @@ export default class Card extends Component{
 
     componentDidMount(){
         const {out} = this.props
-        if(out){
-            this.setState({
-                isDisabled: true
-            })
-        } else{
-            this.setState({
-                isDisabled: false
-            })
-        }
+        this.setState({
+            isDisabled: out
+        })
     }
     render(){
         const {weight, inside, portions, mouses, p3, about} = this.props
         const border_color = this.state.isDisabled ? 'card_border is_disabled' : (this.state.isSelected ? (this.state.isHovered ? 'card_border is_selected-hovered' : 'card_border is_selected') : (this.state.isHovered ? 'card_border is_hovered' : 'card_border is_default')) 
         const ellips = this.state.isDisabled ? 'card_weight is_disabled' : (this.state.isSelected ? (this.state.isHovered ? 'card_weight is_selected-hovered' : 'card_weight is_selected') : (this.state.isHovered ? 'card_weight is_hovered' : 'card_weight is_default')) 
-    const undercard = this.state.isDisabled ? <p className="card_outer-text">Печалька, с {inside} закончился</p> : (this.state.isSelected ? <p className="card_outer-text">{about}</p> : <p className="card_outer-text">Чего сидишь? Порадуй котэ, <span onClick={() => this.cardClick()} className="card_outer-text__link">купи.</span></p>)
+        const card_opacity = this.state.isDisabled ? 'card is-disabled_opacity' : 'card'
+        const undercard = this.state.isDisabled ? <p className="card_outer-text__disabled">Печалька, с {inside} закончился.</p> : (this.state.isSelected ? <p className="card_outer-text">{about}</p> : <p className="card_outer-text">Чего сидишь? Порадуй котэ, <span onClick={() => this.cardClick()} className="card_outer-text__link">купи.</span></p>)
         
         return(
             <div className="card_wrapper">
                 <div onClick={() => this.cardClick()} onMouseEnter={() => this.cardHover()} onMouseLeave={() => this.cardLeave()} className={border_color}>
-                    <div className="card">
+                    <div className={card_opacity}>
                         <div className='card_info'>
                         <p>Сказочное заморское яство</p>
                         <h2>Нямушка</h2>
