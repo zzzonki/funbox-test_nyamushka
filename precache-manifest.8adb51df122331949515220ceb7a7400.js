afd708973055b823fa56f0e75822de39cf80ee7b
zzzonki <zzzonki@gmail.com>
2020-09-22 21:25:13 +0300

    Updates

---
+++ precache-manifest.8adb51df122331949515220ceb7a7400.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c4143c961a5be7505b1c35d99d59f6",
+    "url": "/funbox-test_nyamushka/index.html"
+  },
+  {
+    "revision": "7fc7837b11613b9018ce",
+    "url": "/funbox-test_nyamushka/static/css/main.e7a484cd.chunk.css"
+  },
+  {
+    "revision": "4d8c6db4d40a64ee0a0c",
+    "url": "/funbox-test_nyamushka/static/js/2.7c236ac3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/funbox-test_nyamushka/static/js/2.7c236ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc7837b11613b9018ce",
+    "url": "/funbox-test_nyamushka/static/js/main.161e7ea4.chunk.js"
+  },
+  {
+    "revision": "41a429bcdfaf8f03cf89",
+    "url": "/funbox-test_nyamushka/static/js/runtime-main.21dc4d73.js"
+  },
+  {
+    "revision": "26b0c4141e1e5726b2075cbf79945548",
+    "url": "/funbox-test_nyamushka/static/media/funbox_logo.26b0c414.png"
+  }
+]);
